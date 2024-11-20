a3b385b48350f351276d16039aba7596f68b7615
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2024-11-20 15:04:44 +0100

    Decision finale les couleurs seront stockés dans les points

--- src/Point.java
+++ src/Point.java
@@ -1,21 +1,24 @@
-
-public class Point{
+public class Point {
 
     private int x; // Coordonnée X
     private int y; // Coordonnée Y
+    private char c1, c2, c3, c4; // Couleurs pour les quadrants NO, NE, SE, SO
 
     // Constructeur
-    public Point(int x, int y) {
+    public Point(int x, int y, char c1, char c2, char c3, char c4) {
         this.x = x;
         this.y = y;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.c4 = c4;
     }
 
     // Getters
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public char getC1() { return c1; }
+    public char getC2() { return c2; }
+    public char getC3() { return c3; }
+    public char getC4() { return c4; }
 }

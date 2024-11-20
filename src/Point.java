b6741c47e762da6b6c5db04bca0f1de6fc6ec9ba
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2024-11-20 14:55:33 +0100

    removed color attributes from Point

--- src/Point.java
+++ src/Point.java
@@ -1,21 +1,13 @@
 
 public class Point{
-    //test
+
     private int x; // Coordonnée X
     private int y; // Coordonnée Y
-    private char couleurNO; // Couleur pour la région Nord-Ouest
-    private char couleurNE; // Couleur pour la région Nord-Est
-    private char couleurSE; // Couleur pour la région Sud-Est
-    private char couleurSO; // Couleur pour la région Sud-Ouest
 
     // Constructeur
-    public Point(int x, int y, char couleurNO, char couleurNE, char couleurSE, char couleurSO) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
-        this.couleurNO = couleurNO;
-        this.couleurNE = couleurNE;
-        this.couleurSE = couleurSE;
-        this.couleurSO = couleurSO;
     }
 
     // Getters
@@ -25,21 +17,5 @@ public class Point{
 
     public int getY() {
         return y;
-    }
-
-    public char getCouleurNO() {
-        return couleurNO;
-    }
-
-    public char getCouleurNE() {
-        return couleurNE;
-    }
-
-    public char getCouleurSE() {
-        return couleurSE;
-    }
-
-    public char getCouleurSO() {
-        return couleurSO;
     }
 }

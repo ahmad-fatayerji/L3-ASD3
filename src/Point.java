0d3d8214766e6c5efeb5fbf0d26dcd23cbbb28b2
kyky <kykyh44@gmail.com>
2024-11-20 15:43:43 +0100

    added second constructor

--- src/Point.java
+++ src/Point.java
@@ -14,6 +14,11 @@ public class Point {
         this.c4 = c4;
     }
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     // Getters
     public int getX() { return x; }
     public int getY() { return y; }

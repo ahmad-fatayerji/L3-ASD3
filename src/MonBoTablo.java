921d6025c9eb14cda8d47ef483f2095c712dd276
kyky <kykyh44@gmail.com>
2024-11-20 20:15:47 +0100

    changement du main

--- src/MonBoTablo.java
+++ src/MonBoTablo.java
@@ -1,34 +1,33 @@
-import java.awt.Point;
-import java.io.IOException;
+
+
 
 
 public class MonBoTablo {
     public static void main(String[] args) {
+        // Définir les points de base du Quadtree
+        Point basGauche = new Point(0, 0);
+        Point hautDroit = new Point(8, 8);
 
-    //     try {
-    //         // Définir les points de base du Quadtree
-    //         Point basGauche = new Point(0, 0);
-    //         Point hautDroit = new Point(8, 8);
+        // Créer un Quadtree de base avec une couleur par défaut
+        Quadtree quadtree = new Quadtree(basGauche, hautDroit, 'N');
 
-    //         // Créer un Quadtree de base avec une couleur par défaut
-    //         Quadtree quadtree = new Quadtree(new Point(0, 0), new Point(8, 8),'N');
+        // Tester la division du Quadtree
+        System.out.println("=== Division ===");
+        Point divisionPoint = new Point(4, 4);
+        quadtree.diviser(divisionPoint);
+        System.out.println("Division effectuée : 4 quadrants créés.");
 
-    //         // Tester la division du Quadtree
-    //         System.out.println("=== Division ===");
-    //         quadtree.diviser(new Point(4, 4)); 
-    //         System.out.println("Division effectuée : 4 quadrants créés.");
+        // Tester la recherche dans le Quadtree
+        System.out.println("\n=== Recherche ===");
+        Point recherchePoint = new Point(6, 6);
+        Quadtree regionTrouvee = quadtree.searchQTree(recherchePoint.getX(), recherchePoint.getY());
+        System.out.println("Point trouvé dans la région : (" +
+                regionTrouvee.getPointX().getX() + ", " + regionTrouvee.getPointX().getY() + ") -> (" +
+                regionTrouvee.getPointY().getX() + ", " + regionTrouvee.getPointY().getY() + ")");
 
-    //         // Tester la recherche dans le Quadtree
-    //         System.out.println("\n=== Recherche ===");
-    //         Point recherchePoint = new Point(6, 6);
-    //         Quadtree regionTrouvee = quadtree.searchQTree(recherchePoint.x, recherchePoint.y);
-    //         System.out.println("Point trouvé dans la région : (" +
-    //             regionTrouvee.getPointX().getX() + ", " + regionTrouvee.getPointX().getY() + ") -> (" +
-    //             regionTrouvee.getPointY().getX() + ", " + regionTrouvee.getPointY().getY() + ")");
-    //     }
-    //     catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    System.out.println("test");
+        // Test supplémentaire
+        System.out.println("\n=== Test supplémentaire ===");
+        quadtree.reColor(6, 6, 'R');
+        System.out.println("Couleur de la région modifiée à 'R'.");
     }
 }
